2c67b239480c1bd04f63ad1d71c3795bfd23994e
Konstantin Barkalov <mail@barkalov.ru>
2023-12-24 18:31:38 +0700

    upd preinit: node and repo

--- preinit.sh
+++ preinit.sh
@@ -2,12 +2,20 @@
 sudo apt update
 sudo apt install git -y
 cd ~
-curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs -y
+
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs -y
 node -v
+
 cd /srv
-git clone https://barkalov@bitbucket.org/barkalov/snowtime.space.git
+git clone https://github.com/konstantinbarkalov/snowtime.space.git
 npm install forever -g
 cd ./snowtime.space
 cd ./standaloneHttpServer
